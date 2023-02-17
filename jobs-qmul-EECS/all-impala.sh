09c59e24613fd597569ba3cc41dbf36f510b2f02
Saied Ghafouri <s.ghafouri@qmul.ac.uk>
2023-02-17 15:14:22 +0000

    all-algorithms-configs-added

--- jobs-qmul-EECS/all-impala.sh
+++ jobs-qmul-EECS/all-impala.sh
@@ -1,15 +1,15 @@
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-p-small --cluster-id 18 --workload-id 0 --series 116
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-cv-small --cluster-id 18 --workload-id 0 --series 116
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-balance-small --cluster-id 18 --workload-id 0 --series 116
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-p-small --cluster-id 18 --workload-id 0 --series 122
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-cv-small --cluster-id 18 --workload-id 0 --series 122
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-balance-small --cluster-id 18 --workload-id 0 --series 122
 
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-p-medium --cluster-id 17 --workload-id 0 --series 116
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-cv-medium --cluster-id 17 --workload-id 0 --series 116
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-balance-medium --cluster-id 17 --workload-id 0 --series 116
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-p-medium --cluster-id 17 --workload-id 0 --series 122
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-cv-medium --cluster-id 17 --workload-id 0 --series 122
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-balance-medium --cluster-id 17 --workload-id 0 --series 122
 
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-p-big --cluster-id 19 --workload-id 0 --series 116
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-cv-big --cluster-id 19 --workload-id 0 --series 116
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-balance-big --cluster-id 19 --workload-id 0 --series 116
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-p-big --cluster-id 19 --workload-id 0 --series 122
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-cv-big --cluster-id 19 --workload-id 0 --series 122
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-balance-big --cluster-id 19 --workload-id 0 --series 122
 
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-p-axtra --cluster-id 19 --workload-id 0 --series 116
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-cv-axtra --cluster-id 19 --workload-id 0 --series 116
-/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-balance-axtra --cluster-id 19 --workload-id 0 --series 116
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-p-axtra --cluster-id 20 --workload-id 0 --series 122
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-cv-axtra --cluster-id 20 --workload-id 0 --series 122
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file IMPALA-balance-axtra --cluster-id 20 --workload-id 0 --series 122
---
+++ jobs-qmul-EECS/all-other.sh
@@ -0,0 +1,19 @@
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file A2C-p-medium --cluster-id 17 --workload-id 0 --series 124
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file A2C-cv-medium --cluster-id 17 --workload-id 0 --series 124
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file A2C-balance-medium --cluster-id 17 --workload-id 0 --series 124
+
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file A3C-p-medium --cluster-id 17 --workload-id 0 --series 124
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file A3C-cv-medium --cluster-id 17 --workload-id 0 --series 124
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file A3C-balance-medium --cluster-id 17 --workload-id 0 --series 124
+
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file DDQN-p-medium --cluster-id 17 --workload-id 0 --series 124
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file DDQN-cv-medium --cluster-id 17 --workload-id 0 --series 124
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file DDQN-balance-medium --cluster-id 17 --workload-id 0 --series 124
+
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file APEX-DDQN-p-medium --cluster-id 17 --workload-id 0 --series 124
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file APEX-DDQN-cv-medium --cluster-id 17 --workload-id 0 --series 124
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file APEX-DDQN-balance-medium --cluster-id 17 --workload-id 0 --series 124
+
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file APEX-DQN-p-medium --cluster-id 17 --workload-id 0 --series 124
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file APEX-DQN-cv-medium --cluster-id 17 --workload-id 0 --series 124
+/homes/sg324/.conda/envs/central/bin/python /homes/sg324/smart-scheduler/experiments/training/train.py --config-file APEX-DQN-balance-medium --cluster-id 17 --workload-id 0 --series 124
